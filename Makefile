4ed7f4e9fac7c3df93f32c7638bae0660c40da9e
Taha Chtaibi <tchtaibi@e2r4p7.1337.ma>
2022-03-03 03:26:18 +0100

    100%

--- Makefile
+++ Makefile
@@ -1,29 +1,44 @@
-NAME = push_swap
-FLAGS = -Wall -Wextra -Werror
-SRC = main.c ft_sort_and_index.c digits_checker.c \
+src = main.c ft_sort_and_index.c digits_checker.c \
 	linked_list.c finder.c moves_p.c sort_100.c \
 	ft_atoi.c moves_r.c sort_2345.c ft_doublecheck.c moves_rr.c \
 	ft_indexing.c moves_s.c stock.c sort_5_norm.c sort_4_norm.c some_tools.c
-BNS_SRC = bonus.c gnl.c gnl_u.c digits_checker.c \
+push_swap = push_swap
+FLAGS = -Wall -Wextra -Werror
+CC = cc
+
+all : push_swap
+	
+$(push_swap): $(src)	
+	@$(CC) $(FLAGS) $(src) -o push_swap
+	@tput setaf 2; echo "PUSH_SWAP IS READY"
+
+clean :
+	@rm -f push_swap
+	@tput setaf 1; echo "CLEAN COMPLET"
+
+fclean : clean
+
+re: fclean all
+
+src_bonus = bns.c gnl.c gnl_u.c digits_checker.c \
 	linked_list.c finder.c moves_p_b.c  \
 	ft_atoi.c moves_r_b.c ft_doublecheck.c moves_rr_b.c \
 	moves_s_b.c stock.c some_tools.c\
 	ft_strcmp.c ko_ok.c
+checker = checker
+FLAGS = -Wall -Wextra -Werror
+CC = cc
 
-all : $(NAME)
+bonus : checker
 
-$(NAME) : $(SRC)
-	@gcc $(FLAGS) $(SRC) -o $(NAME)
-	@tput setaf 2; echo "PUSH_SWAP IS READY"
+$(checker) : $(src_bonus)
+	@$(CC) $(FLAGS) $(src_bonus) -o checker
+	@tput setaf 2; echo "BONUS IS READY"
 
-bonus : $(BNS_SRC)
-	@gcc $(FLAGS) $(BNS_SRC) -o checker
-	@tput setaf 2; echo "CHECKER IS READY"
-
-
-clean:
-	@rm -f $(NAME)
+clean_bonus :
+	@rm -f checker
 	@tput setaf 1; echo "CLEAN COMPLET"
-fclean: clean
+
+fclean : clean
 
 re: fclean all
--- sort_100.c
+++ sort_100.c
@@ -12,6 +12,50 @@
 
 #include "push_swap.h"
 
+int	push_index_b_2(t_node **stack_a, t_node **stack_b, int b, t_va va)
+{
+	while (b > va.size_stack / 2)
+	{
+		if ((*stack_a)->index >= va.min && \
+			(*stack_a)->index <= va.max)
+		{
+			ft_pb(stack_a, stack_b);
+			if ((*stack_b)->index < va.med)
+				ft_rb(stack_b);
+			va.push--;
+			if (!va.push)
+				break ;
+		}
+		else
+			ft_rra(stack_a);
+		b--;
+		va.size_stack = ft_linkedlen(*stack_a);
+	}
+	return (va.push);
+}
+
+int	push_index_b_3(t_node **stack_a, t_node **stack_b, int b, t_va va)
+{
+	while (b <= va.size_stack / 2)
+	{
+		if ((*stack_a)->index >= va.min && \
+			(*stack_a)->index <= va.max)
+		{
+			ft_pb(stack_a, stack_b);
+			if ((*stack_b)->index < va.med)
+				ft_rb(stack_b);
+			va.push--;
+			if (va.push == 0)
+				break ;
+		}
+		else
+			ft_ra(stack_a);
+		b++;
+		va.size_stack = ft_linkedlen(*stack_a);
+	}
+	return (va.push);
+}
+
 void	push_index_b(t_node **stack_a, t_node **stack_b, int b)
 {
 	t_va	va;
@@ -21,7 +65,7 @@ void	push_index_b(t_node **stack_a, t_node **stack_b, int b)
 	{
 		va.push = (((va.size_stack - 5) / 3) + 1);
 		if (va.size_stack >= 100)
-			va.push = (((va.size_stack - 5) / 4) + 1);
+			va.push = (((va.size_stack - 5) / 5) + 1);
 		va.min = ft_min_of_stack(*stack_a);
 		va.max = ((va.min + va.push) - 1);
 		va.med = ((va.min + va.max) / 2);
@@ -29,49 +73,45 @@ void	push_index_b(t_node **stack_a, t_node **stack_b, int b)
 		{
 			b = ft_search_min_of_range(*stack_a, va.min, va.max);
 			if (b > va.size_stack / 2)
-			{
-				while (b > va.size_stack / 2)
-				{
-					if ((*stack_a)->index >= va.min && \
-						(*stack_a)->index <= va.max)
-					{
-						ft_pb(stack_a, stack_b);
-						if ((*stack_b)->index < va.med)
-							ft_rb(stack_b);
-						va.push--;
-					}
-					else
-						ft_rra(stack_a);
-					b--;
-					va.size_stack = ft_linkedlen(*stack_a);
-				}
-			}
+				va.push = push_index_b_2(stack_a, stack_b, b, va);
 			else if (b <= va.size_stack / 2)
-			{
-				while (b <= va.size_stack / 2)
-				{
-					if ((*stack_a)->index >= va.min && \
-						(*stack_a)->index <= va.max)
-					{
-						ft_pb(stack_a, stack_b);
-						if ((*stack_b)->index < va.med)
-							ft_rb(stack_b);
-						va.push--;
-					}
-					else
-						ft_ra(stack_a);
-					b++;
-					va.size_stack = ft_linkedlen(*stack_a);
-				}
-			}
+				va.push = push_index_b_3(stack_a, stack_b, b, va);
 		}
 		va.size_stack = ft_linkedlen(*stack_a);
 	}
 }
 
-void	sort_100(t_node **stack_a, t_node **stack_b)
+void	sort_100_2(t_node **stack_a, t_node **stack_b, int s_stack)
 {
 	int	i;
+
+	i = search_place_index((*stack_a)->index - 1,*stack_b);
+	if (i > s_stack / 2)
+	{
+		while (i > s_stack / 2)
+		{
+			if ((*stack_a)->index -1 == (*stack_b)->index)
+				break ;
+			ft_rrb(stack_b);
+			i--;
+			s_stack = ft_linkedlen(*stack_b);
+		}
+	}
+	else if (i <= s_stack / 2)
+	{
+		while (i <= s_stack / 2)
+		{
+			if ((*stack_a)->index -1 == (*stack_b)->index)
+				break ;
+			ft_rb(stack_b);
+			i++;
+			s_stack = ft_linkedlen(*stack_b);
+		}
+	}
+}
+
+void	sort_100(t_node **stack_a, t_node **stack_b)
+{
 	int	s_stack;
 
 	push_index_b(stack_a, stack_b, 0);
@@ -83,29 +123,7 @@ void	sort_100(t_node **stack_a, t_node **stack_b)
 			ft_pa(stack_a, stack_b);
 		while ((*stack_a)->index - 1 != (*stack_b)->index)
 		{
-			i = search_place_index((*stack_a)->index - 1,*stack_b);
-			if (i > s_stack / 2)
-			{
-				while (i > s_stack / 2)
-				{
-					if ((*stack_a)->index -1 == (*stack_b)->index)
-						break ;
-					ft_rrb(stack_b);
-					i--;
-					s_stack = ft_linkedlen(*stack_b);
-				}
-			}
-			else if (i <= s_stack / 2)
-			{
-				while (i <= s_stack / 2)
-				{
-					if ((*stack_a)->index -1 == (*stack_b)->index)
-						break ;
-					ft_rb(stack_b);
-					i++;
-					s_stack = ft_linkedlen(*stack_b);
-				}
-			}
+			sort_100_2(stack_a, stack_b, s_stack);
 		}
 		s_stack = ft_linkedlen(*stack_b);
 	}
