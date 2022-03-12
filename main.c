3ef2e2f382b84434bfc2a162357a5bbc18087228
Taha Chtaibi <tchtaibi@e2r6p12.1337.ma>
2022-03-12 23:26:15 +0100

    11:26

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ t_node	*split_args(char **av, int ac, char **str, t_node *stack_a)
 	(void)ac;
 	while (av[i])
 	{
-		if (av[i][0] == ' ')
+		if (av[i][0] == ' ' && !av[i][1])
 			ft_exit();
 		str = ft_split(av[i], ' ');
 		if (!ft_dgts_check(str))
